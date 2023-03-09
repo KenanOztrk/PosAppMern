12337a05406d3ea54f7fc505943ed8dcff9a2bd3
ozturk.kenan1@outlook.com <ozturk.kenan1@outlook.com>
2023-03-09 11:06:04 +0300

    statistics page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter, Routes, Route} from "react-router-dom";
 import CartPage from "./pages/CartPage";
+import CustomerPage from "./pages/CustomerPage";
 import HomePage from "./pages/HomePage";
 import InvoicePage from "./pages/InvoicePage";
+import StatisticPage from "./pages/StatisticPage";
 
 function App() {
   return (
@@ -11,8 +13,8 @@ function App() {
           <Route path="/" element = { <HomePage /> } />
           <Route path="/cart" element = { <CartPage /> } />
           <Route path="/invoices" element = { <InvoicePage/> } />
-
-
+          <Route path="/customers" element = { <CustomerPage/> } />
+          <Route path="/statistic" element = { <StatisticPage/> } />
         </Routes>
       </BrowserRouter>
     </>
